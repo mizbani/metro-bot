d20ee78d15312ecc1bb1f44b770d23d2596e4317
Reza Mizbani <mizbani.reza@gmail.com>
2023-05-22 23:26:36 +0330

    evn port

--- bot.js
+++ bot.js
@@ -105,8 +105,9 @@ function findStationByName(stationName) {
   return station;
 }
 
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log("Server started on port " + port);
 });
 
 module.exports = app;
